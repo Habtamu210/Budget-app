612ba51b266eed4151fa7ae4489ac4ad65b7ba3a
Habtamu210 <beleyaha@gmail.com>
2023-06-14 16:11:04 -0700

    Create controller

--- app/controllers/catagories_controller.rb
+++ app/controllers/catagories_controller.rb
@@ -1,40 +1,67 @@
 class CatagoriesController < ApplicationController
+  before_action :set_category, only: %i[edit update destroy]
+  before_action :set_user, only: %i[index edit create update destroy]
+
+  # GET /categories or /categories.json
   def index
-    if current_user
-      @categories = Category.includes(:trades).where(user: current_user)
+    if user_signed_in?
+      @page_title = 'Categories'
+      @categories = Category.includes(:expenses).where(user: current_user)
+      @total = Expense.where(user: current_user).sum(&:amount)
     else
-      redirect root_path
+      @page_title = 'Welcome'
+      render :splash
     end
   end
 
-  def show
-    if current_user
-      @category = Category.find(params[:id])
-      trades = @category.trades
-      @trades = trades.sort_by { |t| -t.created_at.to_i }
-    else
-      redirect_to categories_path
-    end
+  # GET /category/new
+  def new
+    @category = Category.new
   end
 
-  def new; end
+  # GET /categories/1/edit
+  def edit; end
 
+  # POST /categories or /categories.json
   def create
-    category = Category.new(category_params)
-    category.user = current_user
-    category.trades = []
-    if category.save
-      flash[:notice] = 'New category was created successfully'
-      redirect_to categories_path
+    @category = Category.new(**category_params)
+    @category.user = current_user
+
+    if @category.save
+      redirect_to categories_url, notice: 'Category was successfully created.'
     else
-      flash[:alert] = 'Error! Category could not create'
-      redirect_to new_category_path
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  # PATCH/PUT /categories/1 or /categories/1.json
+  def update
+    if @category.update(category_params)
+      redirect_to categories_url, notice: 'Category was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /categories/1 or /categories/1.json
+  def destroy
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
   end
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = current_user
+  end
+
+  def set_category
+    @category = set_user.categories.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
   def category_params
-    params.required(:category).permit(:name, :icon)
+    params.require(:category).permit(:name, :icon)
   end
 end
