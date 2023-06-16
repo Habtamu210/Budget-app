ca7a7ea324619ebfa2cd3373a4611360da12760e
Habtamu210 <beleyaha@gmail.com>
2023-06-16 10:34:40 -0700

    config devise, respec and robocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_164103) do
+ActiveRecord::Schema[7.0].define(version: 20_230_616_164_103) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "icon", default: "https://coffee.alexflipnote.dev/random"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.string 'icon', default: 'https://coffee.alexflipnote.dev/random'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_categories_on_user_id'
   end
 
-  create_table "category_expenses", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "expense_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_category_expenses_on_category_id"
-    t.index ["expense_id"], name: "index_category_expenses_on_expense_id"
+  create_table 'category_expenses', force: :cascade do |t|
+    t.bigint 'category_id', null: false
+    t.bigint 'expense_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_category_expenses_on_category_id'
+    t.index ['expense_id'], name: 'index_category_expenses_on_expense_id'
   end
 
-  create_table "expenses", force: :cascade do |t|
-    t.string "name"
-    t.decimal "amount"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_expenses_on_user_id"
+  create_table 'expenses', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'amount'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_expenses_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "categories", "users"
-  add_foreign_key "category_expenses", "categories"
-  add_foreign_key "category_expenses", "expenses"
-  add_foreign_key "expenses", "users"
+  add_foreign_key 'categories', 'users'
+  add_foreign_key 'category_expenses', 'categories'
+  add_foreign_key 'category_expenses', 'expenses'
+  add_foreign_key 'expenses', 'users'
 end
